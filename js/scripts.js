06eebf2e0dde174f493e8fb2ab83d87b823f806d
Elrey Belmonti and Erik Zakrzewski <erik@>
2017-08-16 10:35:26 -0700

    add logic to test or non-alphabetic input

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 // business logic
-
-
+var pigLatin = function translate(word) {
+  debugger;
+  if (word.match(/[^a-z]/)){
+      return word;
+    }
+};
 
 
 // user interface logic
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#pre-latin").submit(function(event) {
     event.preventDefault();
+
+    var wordInput = $("input#english").val();
+    var result = pigLatin(wordInput);
+
+    $(".pLatin").text(result);
 
   });
 });
