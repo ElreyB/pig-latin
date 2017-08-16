0a48cdd79cd1e78f6b9934f9b7ea582a56a5cf32
Elrey Belmonti and Erik Zakrzewski <erik@>
2017-08-16 14:31:06 -0700

    add logic to handle all input according to rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
 // business logic
+var consonants = "";
+var noConsonantsBegin = "";
+var newWord = "";
+
 function removeConsonants(word){
-  var newWord = "";
-  var letters = word.split("");
-  letters.forEach(function(letter){
-    if (letter.match(/[aeiou]/i)){
-      newWord += letter;
-    }
-  });
-  return newWord;
+  newWord = word.match(/([aeiou]([a-z]*))/i);
+  return newWord[0];
+}
+
+function removeQu(word){
+  newWord = word.match(/([aeio]([a-z]*))/i);
+  return newWord[0];
 }
 
 
 var pigLatin = function translate(word) {
-  // debugger;
+  debugger;
   if (word.match(/[^a-z]/i)){
       return word;
-    } else if (word[0].match(/[aeiou]/i)){
-      return word + "way";
-  } else if (word.match(/[^aeiou]/)){
-    var consonants = word.match(/[^aeiou]*/);
-    var pigLatin =
-    console.log(word)
+  } else if (word[0].match(/[aeiou]/i)){
+    return word + "way";
+  }else if (word.match(/[^aeiou]/i)){
+    consonants = word.match(/[^aeiou]*/i);
+    noConsonantsBegin = removeConsonants(word);
+    return noConsonantsBegin + consonants + "way";
+  }else if (word.match(/([^aeiou](qu))/i)){
+    consonants = word.match(/([^aeiou](qu))/i);
+    noConsonantsBegin = removeQu(word);
+    return noConsonantsBegin + consonants + "way";
   }
 };
 
